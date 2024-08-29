89e04e792c12bdaebe24cfb755fb1aa60a10065a
Сергей Политаев <ronin@sonikelf.ru>
2024-08-29 23:35:00 +0300

    Add range slider component to filters

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -3,6 +3,7 @@ import { cn } from '@/lib/utils';
 import { Title } from './title';
 import { FilterCheckbox } from './filter-checkbox';
 import { Input } from '../ui';
+import { RangeSlider } from './range-slider';
 
 interface Props {
 	className?: string;
@@ -28,6 +29,7 @@ export const Filters: React.FC<Props> = ({ className }) => {
 					/>
 					<Input type="number" min={100} max={1000} placeholder="1000" />
 				</div>
+				<RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
 			</div>
 		</div>
 	);
