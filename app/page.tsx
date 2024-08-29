df723f34ca934519331d60d5e0c0f0ac3f6de5ab
Сергей Политаев <ronin@sonikelf.ru>
2024-08-29 23:01:47 +0300

    Add categories and sort components to top bar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import { Button } from '@/components/ui/button';
+import {
+	Categories,
+	Container,
+	SortPopup,
+	Title,
+	TopBar,
+} from '@/components/shared';
 
 export default function Home() {
 	return (
-		<h1>
-			Home
-			<Button variant="outline">Корзина</Button>
-		</h1>
+		<>
+			<Container className="mt-10">
+				<Title text="Все пиццы" size="lg" className="font-extrabold" />
+			</Container>
+			<TopBar />
+		</>
 	);
 }
