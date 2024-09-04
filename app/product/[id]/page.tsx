1b5364604cfff165fb43cd9ac1df9e4066364e88
Сергей Политаев <ronin@sonikelf.ru>
2024-09-04 05:09:17 +0300

    Add product page component with product image and variants components

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,46 @@
-export default function ProductPage({
+import {
+	Container,
+	GroupVariants,
+	ProductImage,
+	Title,
+} from '@/components/shared';
+import { prisma } from '@/prisma/prisma-client';
+import { notFound } from 'next/navigation';
+
+export default async function ProductPage({
 	params: { id },
 }: {
 	params: { id: string };
 }) {
-	return <div>Product {id}</div>;
+	const product = await prisma.product.findFirst({
+		where: { id: Number(id) },
+	});
+
+	if (!product) return notFound();
+	return (
+		<Container className="flex flex-col my-10">
+			<div className="flex flex-1">
+				<ProductImage imageUrl={product.imageUrl} size={40} />
+
+				<div className="w-[490px] bg-[#f7f6f5] p-7">
+					<Title
+						text={product.name}
+						size="md"
+						className="mb-1 font-extrabold"
+					/>
+					<p className="text-gray-400">
+						Lorem, ipsum dolor sit amet consectetur
+					</p>
+					<GroupVariants
+						selectedValue="2"
+						items={[
+							{ name: 'Маленькая', value: '1' },
+							{ name: 'Средняя', value: '2' },
+							{ name: 'Большая', value: '3', disabled: true },
+						]}
+					/>
+				</div>
+			</div>
+		</Container>
+	);
 }
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,3 +1,5 @@
+import { GroupVariants } from './group-variants';
+import { ProductImage } from '@/components/shared/product-image';
 export { Container } from './container';
 export { Header } from './header';
 export { Title } from './title';
@@ -11,3 +13,5 @@ export { CheckboxFiltersGroup } from './checkbox-filters-group';
 export { ProductCard } from './product-card';
 export { ProductsGroupList } from './products-group-list';
 export { SearchInput } from './search-input';
+export { ProductImage } from './product-image';
+export { GroupVariants } from './group-variants';
