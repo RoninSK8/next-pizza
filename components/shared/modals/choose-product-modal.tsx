5ce442f7955c0b8aba4dd96217b7fe75af0ad9c5
Сергей Политаев <ronin@sonikelf.ru>
2024-09-10 21:56:08 +0300

    Implement closing modal

--- components/shared/modals/choose-product-modal.tsx
+++ components/shared/modals/choose-product-modal.tsx
@@ -1,9 +1,12 @@
+'use client';
+
 import React from 'react';
 import { cn } from '@/lib/utils';
 import { Dialog } from '@/components/ui';
 import { Product } from '@prisma/client';
 import { DialogContent } from '@/components/ui/dialog';
 import { Title } from '../title';
+import { useRouter } from 'next/navigation';
 
 interface Props {
 	product: Product;
@@ -11,15 +14,16 @@ interface Props {
 }
 
 export const ChooseProductModal: React.FC<Props> = ({ className, product }) => {
+	const router = useRouter();
 	return (
-		<Dialog open={Boolean(product)}>
+		<Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
 			<DialogContent
 				className={cn(
 					'p-o w-[1060px] min-h-[500px] bg-white overflow-hidden',
 					className
 				)}
 			>
-				<Title text={product.name}></Title>
+				<Title text={product.name} />
 			</DialogContent>
 		</Dialog>
 	);
