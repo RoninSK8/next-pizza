4122f63f79a32f3751c92ae5e106a55b9baf58a5
Сергей Политаев <ronin@sonikelf.ru>
2024-09-16 23:16:58 +0300

    Add utility functions for cart item price calculation and details retrieval

---
+++ shared/lib/get-cart-details.ts
@@ -0,0 +1,39 @@
+import { Cart } from '@prisma/client';
+import { CartDTO } from '../services/dto/cart.dto';
+import { calcCartItemTotalPrice } from './calc-cart-item-total-price';
+
+export type CartStateItem = {
+	id: number;
+	quantity: number;
+	name: string;
+	price: number;
+	imageUrl: string;
+	pizzaSize?: number | null;
+	pizzaType?: number | null;
+	ingredients: Array<{ name: string; price: number }>;
+};
+
+interface ReturnProps {
+	items: CartStateItem[];
+	totalAmount: number;
+}
+
+export const getCartDetails = (data: CartDTO): ReturnProps => {
+	const items = data.items.map((item) => ({
+		id: item.id,
+		quantity: item.quantity,
+		name: item.productItem.product.name,
+		price: calcCartItemTotalPrice(item),
+		imageUrl: item.productItem.product.imageUrl,
+		pizzaSize: item.productItem.size,
+		pizzaType: item.productItem.pizzaType,
+		ingredients: item.ingredients.map((ingredient) => ({
+			name: ingredient.name,
+			price: ingredient.price,
+		})),
+	}));
+	return {
+		totalAmount: data.totalAmount,
+		items,
+	};
+};
