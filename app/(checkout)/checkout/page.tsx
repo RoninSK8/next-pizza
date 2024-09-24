d95abbe971152f94e9490870995321e2dc23d71c
Сергей Политаев <ronin@sonikelf.ru>
2024-09-24 23:11:30 +0300

    Add right side of checkout with total order sum and checkout item component

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -1,5 +1,12 @@
-import { Container, Title, WhiteBlock } from '@/shared/components/shared';
-import { Input, Textarea } from '@/shared/components/ui';
+import {
+	CheckoutItem,
+	CheckoutItemDetails,
+	Container,
+	Title,
+	WhiteBlock,
+} from '@/shared/components/shared';
+import { Button, Input, Textarea } from '@/shared/components/ui';
+import { ArrowRight, Package, Percent, Truck } from 'lucide-react';
 
 export default function CheckoutPage() {
 	return (
@@ -12,7 +19,16 @@ export default function CheckoutPage() {
 			<div className="flex gap-10">
 				{/* Левая часть */}
 				<div className="flex flex-col gap-10 flex-1 mb-20">
-					<WhiteBlock title="1. Корзина">123123</WhiteBlock>
+					<WhiteBlock title="1. Корзина">
+						<CheckoutItem
+							id={0}
+							details={''}
+							imageUrl={''}
+							name={''}
+							price={0}
+							quantity={0}
+						/>
+					</WhiteBlock>
 					<WhiteBlock title="2. Персональные данные">
 						<div className="grid grid-cols-2 gap-5">
 							<Input name="fistName" placeholder="Имя" className="text-base" />
@@ -41,7 +57,50 @@ export default function CheckoutPage() {
 						</div>
 					</WhiteBlock>
 				</div>
+
 				{/* Правая часть */}
+				<div className="w-[450px]">
+					<WhiteBlock className="p-6 sticky top-4">
+						<div className="flex flex-col gap-1">
+							<span className="text-xl">Итого:</span>
+							<span className="text-[34px] font-extrabold">3506 ₽</span>
+						</div>
+						<CheckoutItemDetails
+							title={
+								<div className="flex items-center">
+									<Package size={18} className="mr-2 text-gray-400" />
+									Стоимость товаров:
+								</div>
+							}
+							value="3506 ₽"
+						/>
+						<CheckoutItemDetails
+							title={
+								<div className="flex items-center">
+									<Percent size={18} className="mr-2 text-gray-400" />
+									Налоги:
+								</div>
+							}
+							value="3506 ₽"
+						/>
+						<CheckoutItemDetails
+							title={
+								<div className="flex items-center">
+									<Truck size={18} className="mr-2 text-gray-400" />
+									Доставка:
+								</div>
+							}
+							value="3506 ₽"
+						/>
+						<Button
+							type="submit"
+							className="w-full h-14 rounded-2xl mt-6 text-base font-bold"
+						>
+							Перейти к оплате
+							<ArrowRight className="w-5 ml-2" />
+						</Button>
+					</WhiteBlock>
+				</div>
 			</div>
 		</Container>
 	);
