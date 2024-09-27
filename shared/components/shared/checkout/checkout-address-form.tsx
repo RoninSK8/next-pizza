9a270ec744122c14695a266e864150fe2efe4a85
Сергей Политаев <ronin@sonikelf.ru>
2024-09-27 16:48:30 +0300

    Add textarea form component and address form validation using dadata

--- shared/components/shared/checkout/checkout-address-form.tsx
+++ shared/components/shared/checkout/checkout-address-form.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import { WhiteBlock } from '..';
-import { Input, Textarea } from '../../ui';
+import { AddressInput, ErrorText, FormTextarea, WhiteBlock } from '..';
+import { Input } from '../../ui';
+import { Controller, useFormContext } from 'react-hook-form';
 
 interface Props {
 	className?: string;
 }
 
 export const CheckoutAddressForm: React.FC<Props> = ({ className }) => {
+	const { control } = useFormContext();
 	return (
 		<WhiteBlock title="3. Адрес доставки" className={className}>
 			<div className="flex flex-col gap-5">
-				<Input
+				<Controller
+					control={control}
 					name="address"
-					placeholder="Введите адрес..."
-					className="text-base"
+					render={({ field, fieldState }) => (
+						<>
+							<AddressInput onChange={field.onChange} />
+							{fieldState.error?.message && (
+								<ErrorText text={fieldState.error.message} />
+							)}
+						</>
+					)}
 				/>
 
-				<Textarea
+				<FormTextarea
 					rows={5}
 					className="text-base"
 					placeholder="Комментарии к заказу"
+					name="comment"
 				/>
 			</div>
 		</WhiteBlock>
