52b17e3db923005dbb6a7ce59b3c47e8987b2465
Сергей Политаев <ronin@sonikelf.ru>
2024-10-02 22:20:59 +0300

    Add route for successfull payment case - update order status at backend

---
+++ app/api/checkout/callback/route.ts
@@ -0,0 +1,37 @@
+import { PaymentCallbackData } from '@/@types/yookassa';
+import { prisma } from '@/prisma/prisma-client';
+import { OrderStatus } from '@prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+	try {
+		const body = (await req.json()) as PaymentCallbackData;
+
+		const order = await prisma.order.findFirst({
+			where: {
+				id: Number(body.object.metadata.order_id),
+			},
+		});
+
+		if (!order) {
+			return NextResponse.json({ error: 'Order not found' });
+		}
+
+		const isSucceeded = body.object.status === 'succeeded';
+
+		await prisma.order.update({
+			where: {
+				id: order.id,
+			},
+			data: {
+				status: isSucceeded ? OrderStatus.SUCCEEDED : OrderStatus.CANCELLED,
+			},
+		});
+
+		// const items = order?.items as unknown as CartItemDTO[];
+	} catch (error) {
+		console.log('[CHECKOUT_CALLBACK] error', error);
+
+		return NextResponse.json({ error: 'Server error' });
+	}
+}
