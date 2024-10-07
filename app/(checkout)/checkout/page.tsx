f9d50e3533204bc5b392f487150b4e36d920fbe0
Сергей Политаев <ronin@sonikelf.ru>
2024-10-07 23:21:37 +0300

    Implement auto-fill of checkout form with user profile data

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -15,12 +15,15 @@ import { useCart } from '@/shared/hooks';
 
 import { createOrder } from '@/app/actions';
 import toast from 'react-hot-toast';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { Api } from '@/shared/services/api-client';
 
 export default function CheckoutPage() {
 	const [submitting, setSubmitting] = useState(false);
 	const { totalAmount, items, updateItemQuantity, removeCartItem, loading } =
 		useCart();
+	const { data: session } = useSession();
 
 	const form = useForm<CheckoutFormValues>({
 		resolver: zodResolver(checkoutFormSchema),
@@ -33,6 +36,19 @@ export default function CheckoutPage() {
 			comment: '',
 		},
 	});
+
+	useEffect(() => {
+		async function fetchUserInfo() {
+			const data = await Api.auth.getMe();
+			const [firstName, lastName] = data.fullName.split(' ');
+			form.setValue('firstName', firstName);
+			form.setValue('lastName', lastName);
+			form.setValue('email', data.email);
+		}
+		if (session) {
+			fetchUserInfo();
+		}
+	}, [session]);
 
 	const onSubmit = async (data: CheckoutFormValues) => {
 		try {
---
+++ app/api/auth/me/route.ts
@@ -0,0 +1,34 @@
+import { prisma } from '@/prisma/prisma-client';
+import { getUserSession } from '@/shared/lib';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+	try {
+		const user = await getUserSession();
+		if (!user) {
+			return NextResponse.json(
+				{ message: 'Вы не авторизованы' },
+				{ status: 401 }
+			);
+		}
+
+		const data = await prisma.user.findUnique({
+			where: {
+				id: Number(user?.id),
+			},
+			select: {
+				fullName: true,
+				email: true,
+				password: false,
+			},
+		});
+
+		return NextResponse.json(data);
+	} catch (error) {
+		console.log(error);
+		return NextResponse.json(
+			{ message: '[USER_ME] Server error' },
+			{ status: 500 }
+		);
+	}
+}
