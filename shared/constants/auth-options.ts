1f9f458437882171c544d282cba8bc876cb553c9
Сергей Политаев <ronin@sonikelf.ru>
2024-10-08 22:46:10 +0300

    Implement logging in using Google

--- shared/constants/auth-options.ts
+++ shared/constants/auth-options.ts
@@ -1,5 +1,6 @@
 import { AuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 import { prisma } from '@/prisma/prisma-client';
@@ -8,6 +9,11 @@ import { UserRole } from '@prisma/client';
 
 export const authOptions: AuthOptions = {
 	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID || '',
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+		}),
+
 		GitHubProvider({
 			clientId: process.env.GITHUB_ID as string,
 			clientSecret: process.env.GITHUB_SECRET as string,
