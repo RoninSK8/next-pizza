732a74ec2487fe69aa1ba28ecc64e6eeeb790128
Сергей Политаев <ronin@sonikelf.ru>
2024-10-18 19:15:33 +0300

    Refactor product card styles and fix error with filtering by price

--- shared/components/shared/product-card.tsx
+++ shared/components/shared/product-card.tsx
@@ -27,7 +27,11 @@ export const ProductCard: React.FC<Props> = ({
 		<div className={className}>
 			<Link href={`/product/${id}`}>
 				<div className="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
-					<img className="w-[215px] h-[215px]" src={imageUrl} alt={name} />
+					<img
+						className="min-w-52 min-h-52 w-[215px] h-[215px]"
+						src={imageUrl}
+						alt={name}
+					/>
 				</div>
 				<Title text={name} size="sm" className="mb-1 mt-3 font-bold" />
 				<p className="text-sm text-gray-400">
