1bc7f98db9521892231a24bf65622b40c2ee6f56
Сергей Политаев <ronin@sonikelf.ru>
2024-10-31 01:58:59 +0300

    Fix case sensitive products search
    A workaround in order to use case-insensitive search, which breaks when Vercel is dealing with cyrillic text

--- app/api/products/search/route.ts
+++ app/api/products/search/route.ts
@@ -1,15 +1,21 @@
 import { prisma } from '@/prisma/prisma-client';
+import { capitalizeLowercaseText } from '@/shared/lib/capitalize-lowercase-text';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
 	const query = request.nextUrl.searchParams.get('query') || '';
 
+	// Need this in order to use case-insensitive search, which breaks when Vercel is dealing with cyrillic text
+	const capitalizedQuery = capitalizeLowercaseText(query);
+	const lowercaseQuery = query.toLocaleLowerCase();
+
 	const products = await prisma.product.findMany({
 		where: {
-			name: {
-				contains: query,
-				mode: 'insensitive',
-			},
+			OR: [
+				{ name: { contains: query, mode: 'insensitive' } },
+				{ name: { contains: capitalizedQuery, mode: 'insensitive' } },
+				{ name: { contains: lowercaseQuery, mode: 'insensitive' } },
+			],
 		},
 		take: 5,
 	});
